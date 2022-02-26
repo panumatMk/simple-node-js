ba983c2a98d87a4baeb99082ddb699de9f31d798
panumatMk <panumat.mk@gmail.com>
2022-02-27 00:45:19 +0700

    add sse

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const app = require('express')();
 const cors = require('cors');
 const PORT = process.env.PORT || 3333;
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://git.heroku.com/tranquil-stream-88961.git'
+}));
 
 app.get('/', (req, res) => {
     res.status(200).send('hello world')
@@ -14,6 +16,34 @@ app.get('/tshirt', (req, res) => {
         size: 'large'
     })
 });
+
+var clientId = 0;
+var clients = {}; // <- Keep a map of attached clients
+
+// Called once for each new client. Note, this response is left open!
+app.get('/events/', function (req, res) {
+    req.socket.setTimeout(Number.MAX_VALUE);
+    res.writeHead(200, {
+        'Content-Type': 'text/event-stream', // <- Important headers
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive'
+    });
+    res.write('\n');
+    (function (clientId) {
+        clients[clientId] = res; // <- Add this client to those we consider "attached"
+        req.on("close", function () {
+            delete clients[clientId]
+        }); // <- Remove this client when he disconnects
+    })(++clientId)
+});
+
+setInterval(function () {
+    var msg = Math.random();
+    // console.log("Clients: " + Object.keys(clients) + " <- " + msg);
+    for (clientId in clients) {
+        clients[clientId].write("data: " + msg + "\n\n"); // <- Push a message to a single attached client
+    };
+}, 2000);
 
 app.listen(
     PORT,
---
+++ readme.md
@@ -0,0 +1,7 @@
+heroku login
+
+heroku git:remote --app example-project
+
+git push heroku main
+
+heroku open
